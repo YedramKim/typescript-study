3e05a092639402fc3723e417e4062f0aa5d11cf8
YedarmKim <oncoder@naver.com>
2017-11-04 22:56:24 +0900

    11-4

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var module_1 = require("./module/module");
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+console.log(greeter(user));
+console.log(module_1.num);
